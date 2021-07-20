7c330e749ceda422b4ee4f43b8543fcf6cb14c86
Cristofer <cristofer.e.villegas.gonzalez@intel.com>
2021-07-19 22:48:12 -0600

    Itunes RSS feed working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route, useLocation } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import ItemDetails from "./components/Items/ItemDetails";
+import { Fragment, useState } from "react";
+import Layout from './components/UI/Layout'
+import { getItunesItem } from './services/itunes.service'
+import Header from "./components/Header/Header";
+import NotFound from "./components/UI/NotFound";
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [items, setItems] = useState([]);
+  const loadItem = (id) => {
+    setLoading(true)
+    getItunesItem(id).then(list => {
+      setItems(list)
+      setLoading(false)
+    })
+  };
+
+  const location = useLocation()
+
+  const getItemIndex = () => {
+    const pathname = location.pathname
+    return +pathname.split('/')[2]
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout header={<Header> iTunes RSS Feed </Header>} body={
+      <Fragment>
+        <Switch>
+          <Route exact path="/itemdetails/:id">
+            {items && items.length > 0 ? items.filter( (item, index) => {
+              return index === getItemIndex()
+            }).map( (item, index )=> {
+              return <ItemDetails key={index} name={item.name} artistName={item.artistName} artworkUrl100={item.artworkUrl100} releaseDate={item.releaseDate} kind={item.kind}></ItemDetails>
+            }) : <NotFound></NotFound>}
+            
+          </Route>
+          <Route exact path={["/home", "/"]}>
+            <LandingPage
+            loading={loading}
+              onSetItem={loadItem}
+              items={items}
+            ></LandingPage>
+          </Route>
+        </Switch>
+      </Fragment>
+    } />
+
   );
+
 }
 
 export default App;
